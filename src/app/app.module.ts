13de4b70fc8e66da26559d418b1cec6d8c69e700
Nurlan Mirzayev <Nurlan_Mirzayev@epam.com>
2018-06-05 11:33:34 +0200

    Added blank components with ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ToolboxComponent } from './toolbox/toolbox.component';
+import { ListComponent } from './list/list.component';
+import { CourseComponent } from './course/course.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    ToolboxComponent,
+    ListComponent,
+    CourseComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
