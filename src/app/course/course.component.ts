a320ab51e0c57111a5ecc15e0982afcf4d4cd7f8
Nurlan Mirzayev <nurlanmirzay@gmail.com>
2018-07-26 17:52:55 +0200

    create course fixed

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
 import { CoursesService } from '../home/courses.service';
 import { CourseInterface } from '../shared/course-interface';
 
@@ -20,18 +21,21 @@ export class CourseComponent implements OnInit {
     liked: false,
     topRated: false,
   };
+  public _creationDate: any;
 
   constructor(private coursesService: CoursesService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.course = Object.assign({}, this.course, this.coursesService.getById(this.route.snapshot.paramMap.get('id')));
+    // this._creationDate = moment(this.course.creationDate).format('YYYY-MM-DD');
+    this._creationDate = this.course.creationDate;
   }
 
   update() {
-    if (this.course.id) {
-      this.coursesService.onUpdate(this.course);
-    } else {
+    if (this.course.id === null) {
       this.coursesService.onCreate(this.course);
+    } else {
+      this.coursesService.onUpdate(this.course);
     }
     this.router.navigate(['/']);
   }
