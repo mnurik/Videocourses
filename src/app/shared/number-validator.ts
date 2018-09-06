8386a8583855efeb20091546533ebea5a1f09b51
Nurlan <nurlanmirzay@gmail.com>
2018-09-07 01:45:36 +0200

    fixed type for NumberValidator

--- src/app/shared/number-validator.ts
+++ src/app/shared/number-validator.ts
@@ -1,4 +1,4 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export const NumberValidator = (control: AbstractControl): { [key: string]: any } | null =>
+export const NumberValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null =>
   /^[0-9]*$/g.test(control.value) ? null : { notNumber: { value: control.value } };
